bfde5d9414cc46d2c7d20cc7b40baa3133bf72a8
allen <74865251a@gmail.com>
2023-07-23 13:07:02 +0800

    add

--- mydb.py
+++ mydb.py
@@ -2,3 +2,24 @@
 # pip isntall mysql
 # pip install mysql-connector
 # pip install mysql-connector-python
+
+import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = os.getenv("MYSQL_PASSWORD")
+    
+    )
+
+# prepare a cursor object, for search create obtain data
+cursorObject = dataBase.cursor()
+
+# Create a database 
+cursorObject.execute("CREATE DATABASE crm_data")
+
+print("Successful create database name : crm_data !")
+
