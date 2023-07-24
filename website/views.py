58c233527d58335597d3c69ffbf34e997deda92f
allen <74865251a@gmail.com>
2023-07-25 00:01:31 +0800

    add delete record

--- website/views.py
+++ website/views.py
@@ -66,3 +66,14 @@ def customer_record(request, pk):
     else:
         messages.success(request, "You must login to view that page!!!")
         return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Records deleted Successfully !!")
+        return redirect('home')
+    else:
+        messages.success(request, "You must login to do that!!!")
+        return redirect('home')
